24d06c26d5aac1c1497af673125c2ff8f6bb1f2a
Flight <vladimirborisiuk@gmail.com>
2024-09-07 23:46:30 +0300

    feat: reviews crud;

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
@@ -16,4 +17,18 @@ class Review extends Model
         'product_id',
         'is_active'
     ];
+
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
